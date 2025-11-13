a2077bddd9da4eaaa91f2117c3d84598506b4a04
iamjiamingliu <57842784+iamjiamingliu@users.noreply.github.com>
2025-11-12 19:43:47 -0800

    uhm will this work?

--- backend/server.py
+++ backend/server.py
@@ -12,7 +12,7 @@ CORS(app)
 # API endpoint!
 @app.get("/api/your-mom")
 def home():
-    return Response("Error: file too big!", status=413)
+    return Response("IS PHAT", status=413)
 
 # Place endpoints in this space; they define functions/routes the app will save and look out for when requests come in
 # It'll parse and match accordingly; otherwise, it'll reject with a 404 error
